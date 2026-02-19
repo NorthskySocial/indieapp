905f3154f593b1988b8691cf8228118060eed63e
Hilke Ros <>
2026-02-19 13:55:25 +0100

    Switch to theme to include atoms

--- src/alf/index.tsx
+++ src/alf/index.tsx
@@ -9,7 +9,7 @@ import {
   setFontScale as persistFontScale,
 } from '#/alf/fonts'
 import {themes} from '#/alf/themes'
-import {indiePalette} from '#/indie-settings/palette'
+import {indieTheme} from '#/indie-settings/theme'
 import {type Device} from '#/storage'
 
 export {
@@ -128,9 +128,17 @@ export function useAlf() {
 
 export function useTheme(theme?: ThemeName) {
   const alf = useAlf()
-  alf.theme.palette = {
-    ...alf.theme.palette,
-    ...indiePalette,
+  const {palette, atoms} = indieTheme
+  alf.theme = {
+    ...alf.theme,
+    palette: {
+      ...alf.theme.palette,
+      ...palette,
+    },
+    atoms: {
+      ...alf.theme.atoms,
+      ...atoms,
+    },
   }
 
   return React.useMemo(() => {
--- src/indie-settings/palette.ts
+++
@@ -1,9 +0,0 @@
-import palette from './northskyPalette'
-
-const blueskyPalette = {
-  primary_500: '#006AFF', // Primary button background, link texts
-}
-
-const indiePalette = palette || blueskyPalette
-
-export {indiePalette}

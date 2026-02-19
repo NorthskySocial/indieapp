3a4f458bcf04e7437ff7c77c7ddf421802292283
Hilke Ros <>
2026-02-19 13:55:25 +0100

    Switch to theme to include atoms

---
+++ src/indie-settings/theme.ts
@@ -0,0 +1,11 @@
+import theme from './northskyTheme'
+
+const blueskyTheme = {
+  palette: {
+    primary_500: '#006AFF', // Primary button background, link texts
+  },
+}
+
+const indieTheme = theme || blueskyTheme
+
+export {indieTheme}

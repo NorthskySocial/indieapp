11f20313956ef175b4bd0ae394dc3020be8e14af
Hilke Ros <>
2026-04-17 16:04:20 +0200

    Add more northsky styling

--- src/indie-settings/northskyTheme.ts
+++ src/indie-settings/northskyTheme.ts
@@ -1,17 +1,32 @@
 export const northskyDarkTheme = {
   palette: {
+    primary_25: 'red',
+    primary_50: 'yellow',
+    primary_100: '#38175A', // Hover background icons
+    primary_200: '#7780DC', // Disabled button background
+    primary_300: '#59B2CF', // Hover language selector
     primary_500: '#9A45EC', // Primary button background, link texts
+    primary_600: '#59B2CF', // Hover background primary button
   },
   atoms: {
     bg: {
       backgroundColor: '#1F0B35',
     },
+    bg_contrast_25: {
+      backgroundColor: '#38175A', // hover on clickable items
+    },
+    bg_contrast_50: {
+      backgroundColor: 'blue', // form background, hover clickable areas, secondary button
+    },
   },
 }
 
+// #122136 is new notficaitions background
+
 export const northskyLightTheme = {
   palette: {
-    primary_500: '#2AFBBA', // Primary button background, link texts
+    primary_500: '#9A45EC', // Primary button background, link texts
+    primary_600: '#59B2CF',
   },
   atoms: {
     bg: {

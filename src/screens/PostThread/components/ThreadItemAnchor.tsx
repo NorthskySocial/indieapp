c2f76533ad46d1daf079665e7ee85e2ea598d74c
Evelyn Osman <ashex@chipnick.com>
2026-04-17 17:37:13 +0200

    Format fix

--- src/screens/PostThread/components/ThreadItemAnchor.tsx
+++ src/screens/PostThread/components/ThreadItemAnchor.tsx
@@ -372,8 +372,7 @@ const ThreadItemAnchorInner = memo(function ThreadItemAnchorInner({
                           a.py_2xs,
                           a.ml_2xs,
                         ]}>
-                        <Text
-                          style={[t.atoms.text, a.text_xs, a.font_medium]}>
+                        <Text style={[t.atoms.text, a.text_xs, a.font_medium]}>
                           {authorShadow.pronouns}
                         </Text>
                       </View>
